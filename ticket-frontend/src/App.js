c83ae6feb5d4b617f3048b49bee8aa9a1cf1b344
Sarah O'Dell <sarah.mo26@gmail.com>
2021-05-30 12:14:58 -0600

    react app running

--- ticket-frontend/src/App.js
+++ ticket-frontend/src/App.js
@@ -1,11 +1,52 @@
+import React, { Component } from 'react';
+import Tickets from './components/Tickets'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+const baseURL = 'https://localhost:8000'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      ticketList: []
+    }
+  }
+
+
+//fetch tickets
+getTickets = () => {
+  fetch(baseURL + '/api/v1/tickets', {
+    credentials: 'include'
+  })
+  .then(res => {
+    if(res.status == 200 || res.status == 201) {
+      return res.json()
+    }
+  }).then(data => {
+    this.setState({
+      ticketList: data
+    })
+  })
 }
 
-export default App;
+
+
+
+
+
+
+componentDidMount() {
+  this.getTickets()
+}
+
+  render() {
+    return (
+      <div>
+        <Tickets ticketList={this.state.ticketList}/>
+      </div>
+    )
+  }
+}
+
+
---
+++ ticket-frontend/src/components/Tickets.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default function TicketList(props) {
+
+	return (
+		<div>
+			<h2>Active Tickets</h2>
+
+			
+
+		</div>
+	)
+}
