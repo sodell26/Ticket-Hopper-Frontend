316fc1326857c4b0b3be452bad904fbf68902643
Sarah O'Dell <sarah.mo26@gmail.com>
2021-06-03 17:47:12 -0600

    added more lines to the ticket

--- ticket-frontend/src/components/Tickets.js
+++ ticket-frontend/src/components/Tickets.js
@@ -11,9 +11,22 @@ export default function TicketList(props) {
 				return(
 					<div key={oneTicket.id}>
 						<h2>Ticket Number: {oneTicket.id}</h2>
+						{oneTicket.assigned_to == null &&
+							<h3> Assigned to: Unassigned</h3>
+						}
+						{oneTicket.assigned_to != null &&
+							<h3>Assigned to: {oneTicket.assigned_to.username}</h3>
+						}
+
 						<h3>Submitted By: {oneTicket.submitted_by.username}</h3>
 						<h4>Description: {oneTicket.description}</h4>
 						<h4>Notes: {oneTicket.notes}</h4>
+						{oneTicket.team === null &&
+							<h4> Team: Unassigned </h4>
+						}
+						{oneTicket.team != null &&
+							<h4> Team: {oneTicket.team.name}</h4>
+						}
 						<h6>Created: {oneTicket.created}</h6>
 					</div>
 				)
